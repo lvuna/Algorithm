c1e8fc0ba454e069209b4174050955e7328a994a
ShaoKan <tom19991119@gmail.com>
2019-07-17 19:33:52 -0400

    Finished implementing bst_tree

--- py_implement/tree/bst_tree.py
+++ py_implement/tree/bst_tree.py
@@ -1,6 +1,6 @@
-"""The binary search tree implementation"""
+"""The binary search tree implementation, avl included"""
 from numbers import Number
-from typing import Union, List
+from typing import Union
 
 
 class TreeNode:
@@ -11,14 +11,15 @@ class TreeNode:
     right: "TreeNode"
 
     def __init__(self, key: Number, value: Union[object, None]=None) -> None:
+        self.parent = None
         self.left = None
         self.right = None
         self.key = key
         self.value = value
 
-    def search(self, target: Number) -> object:
+    def search(self, target: Number) -> Union["TreeNode", None]:
         if self.key == target:
-            return self.key if self.value is None else self.value
+            return self
         elif self.key < target and self.right is not None:
             return self.right.search(target)
         elif self.key > target and self.left is not None:
@@ -28,13 +29,16 @@ class TreeNode:
             return
 
     def __str__(self) -> str:
+        parent = None
         left_children = None
         right_children = None
         if self.left is not None:
             left_children = self.left.key
         if self.right is not None:
             right_children = self.right.key
-        result = f'Key:{self.key}, Children:{left_children} {right_children}'
+        if self.parent is not None:
+            parent = self.parent.key
+        result = f'Key:{self.key}, Children:{left_children} {right_children}， Parent:{parent}'
         return result
 
 
@@ -53,7 +57,7 @@ class BinarySearchTree:
         return insert(self.root, node)
 
     def delete(self, key: Number) -> None:
-        pass
+        return delete(self.root, key)
 
 
 def visualize(root: "TreeNode") -> None:
@@ -68,14 +72,66 @@ def insert(root: "TreeNode", node: "TreeNode") -> None:
     """Insert the node into BST."""
     if root.key <= node.key:
         if root.right is None:
+            node.parent = root
             root.right = node
         else:
             insert(root.right, node)
     elif root.key > node.key:
         if root.left is None:
+            node.parent = root
             root.left = node
         else:
             insert(root.left, node)
+
+
+def delete(root: "TreeNode", key: Number) -> None:
+    """Delete the node of given if it exists"""
+    target = root.search(key)
+    parent = target.parent
+    if target is None:
+        return
+    if target.left is None and target.right is None:
+        parent_delete(parent, target)
+    elif target.left is None and target.right is not None:
+        parent_delete(parent, target, target.right)
+    elif target.right is None and target.left is not None:
+        parent_delete(parent, target, target.left)
+    else:
+        pred = predecessor(target)
+        target.key = pred.key
+        target.value = pred.value
+        parent_delete(pred.parent, pred)
+
+
+def predecessor(target: "TreeNode") -> "TreeNode":
+    """Find the predecessor of the target"""
+    node = target.right
+    while node.left is not None:
+        node = node.left
+    return node
+
+
+def parent_delete(parent: "TreeNode", target: "TreeNode", childen: "TreeNode"=None) -> None:
+    if parent.left is None:
+        if childen is None:
+            parent.right = None
+        else:
+            parent.right = childen
+    elif parent.right is None:
+        if childen is None:
+            parent.left = None
+        else:
+            parent.left = childen
+    elif parent.left.key == target.key:
+        if childen is None:
+            parent.left = None
+        else:
+            parent.left = childen
+    else:
+        if childen is None:
+            parent.left = None
+        else:
+            parent.right = childen
 
 
 if __name__ == "__main__":
@@ -89,4 +145,5 @@ if __name__ == "__main__":
     test_BST.insert(node2)
     test_BST.insert(node3)
     test_BST.insert(node4)
+    test_BST.delete(7)
     visualize(test_BST.root)
